e5bdf82b5fb2ffb341a6ab978c35fd9ddc157ad9
Michael McLarnon <mmclar@gmail.com>
2020-11-05 21:18:20 -0500

    cleanup from move to laptop

--- api/data/gtfs/readme.txt
+++ api/data/gtfs/readme.txt
@@ -2,10 +2,12 @@ Unpack GTFS files into this folder:
 agency.txt
 calendar_dates.txt
 calendar.txt
-g_stop_times.txt
 routes.txt
 shapes.txt
 stops.txt
 stop_times.txt
 transfers.txt
 trips.txt
+
+Make g_stop_times.txt:
+grep G\\.\\. stop_times.txt > g_stop_times.txt
--- api/views.py
+++ api/views.py
@@ -9,6 +9,7 @@ from api import models, gtfs
 MTA_INFO_URL = 'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-g'
 MTA_API_KEY = 'lhVgkMCFZk343GkVtKJ0N3ES4fIhz8Rr1jPD4U8A'
 
+
 def _get_time_from_response_content(content):
     try:
         feed = gtfs_realtime_pb2.FeedMessage()
@@ -17,29 +18,31 @@ def _get_time_from_response_content(content):
     except Exception as ex:
         return None
 
+
 def hash_request(method, url, headers):
     return method + url + repr(headers)
 
-def cached_get(method, url, headers={}):
-    hash = hash_request(method, url, headers)
+
+def cached_get(method, url, headers):
+    param_hash = hash_request(method, url, headers)
     try:
-        content = models.ApiResponse.objects.get(hash=hash).content
+        content = models.ApiResponse.objects.get(hash=param_hash).content
     except models.ApiResponse.DoesNotExist:
         response = requests.request(method, url, headers=headers)
         models.ApiResponse(
-            hash=hash,
+            hash=param_hash,
             content=response.content,
         ).save()
         content = response.content
-    return (content, _get_time_from_response_content(content))
+    return content, _get_time_from_response_content(content)
+
 
 def train_locations(request):
-    api_response, rt_timestamp = cached_get('GET', MTA_INFO_URL, headers={
+    api_response, rt_timestamp = cached_get('GET', MTA_INFO_URL, {
         'x-api-key': MTA_API_KEY,
     })
     feed = gtfs_realtime_pb2.FeedMessage()
     feed.ParseFromString(api_response)
-    response = {}
     for trip in feed.entity:
         if trip.HasField('trip_update'):
             print('=============================')
